05cac69c0a3d918da1c2212c70449501d54496b6
Mary Lark <mary@marylark.com>
2018-11-16 19:44:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ function happyHolidayTo(name) {
   return `Happy holidays, ${name}!`;
 }
 
+function happyHolidayTo(name, holiday) {
+  return `Happy ${holiday}, ${name}!`;
+}
+
+
 function holidayCountdown (holiday, days) {
   return `It's ${days} days until ${holiday}!`;
 }
